8ba683878cb5740f002f9f43877d2db35963324f
Michael Sachs <sachsmc@gmail.com>
2015-06-11 12:28:00 -0400

    How to explanation

--- slidersummary.js
+++ slidersummary.js
@@ -3,16 +3,16 @@ var margin = {top: 75, right: 50, bottom: 200, left: 50},
     width = 960 - margin.left - margin.right,
     height = 350 - margin.bottom - margin.top;
 
-var x = d3.scale.linear()
+var xs = d3.scale.linear()
     .domain([0, 100])
     .range([0, width]);
 
 var brush = d3.svg.brush()
-    .x(x)
+    .x(xs)
     .extent([0, 0])
     .on("brush", brushed);
 
-var svg = d3.select("#approaches-to-interactive-graphs").append("svg")
+var svgs = d3.select("#approaches-to-interactive-graphs").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
@@ -32,11 +32,11 @@ var svg = d3.select("#approaches-to-interactive-graphs").append("svg")
 
 	}
 
-svg.append("g")
+svgs.append("g")
     .attr("class", "x axis")
     .attr("transform", "translate(0," + (height+20) + ")")
     .call(d3.svg.axis()
-      .scale(x)
+      .scale(xs)
       .orient("bottom")
 	  .tickValues([15, 40, 60, 85])
       .tickFormat(function(d) { return ticklook(d); })
@@ -46,17 +46,17 @@ svg.append("g")
   .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
     .attr("class", "halo");
 
-svg.append("svg:image").attr("transform", "translate(" + (width-50) + ", -50)")
+svgs.append("svg:image").attr("transform", "translate(" + (width-50) + ", -50)")
    .attr('width', 50)
    .attr('height', 50)
 	.attr("xlink:href","js.png");
 
-svg.append("svg:image").attr("transform", "translate(0, -50)")
+svgs.append("svg:image").attr("transform", "translate(0, -50)")
    .attr('width', 50)
    .attr('height', 50)
 	.attr("xlink:href","Rlogo.png");
 
-var slider = svg.append("g")
+var slider = svgs.append("g")
     .attr("class", "slider")
     .call(brush);
 
@@ -72,34 +72,35 @@ var handle = slider.append("circle")
     .attr("r", 9);
 
 slider
-    .call(brush.event).transition() // gratuitous intro!
+    .call(brush.event)
+  .transition() // gratuitous intro!
     .duration(750)
-    .call(brush.extent([100, 100]))
+    .call(brush.extent([70, 70]))
     .call(brush.event);
 
 
-var Rrect = svg.append("rect").attr("class", "rrect")
+var Rrect = svgs.append("rect").attr("class", "rrect")
 	.attr("width", width/2)
 		.attr("height", height).attr("fill", "#2A6AB1");
 
-var Jsrect = svg.append("rect").attr("class", "jsrect").attr("width", width/2)
+var Jsrect = svgs.append("rect").attr("class", "jsrect").attr("width", width/2)
 	.attr("transform", "translate(" + width/2 + ", 0)")
 		.attr("height", height).attr("fill", "#F0DB4F");
-
-svg.append("")
 
 function brushed() {
   var value = brush.extent()[0];
 
-  //if (d3.event.sourceEvent) { // not a programmatic event
-  //  value = x.invert(d3.mouse(this)[0]);
-  //  brush.extent([value, value]);
-  //}
+if (d3.event.sourceEvent) { // not a programmatic event
+    value = xs.invert(d3.mouse(this)[0]);
+    brush.extent([value, value]);
+}
 
-  d3.select(".rrect").attr("width", x(value));
-  d3.select(".jsrect").attr("width", width - x(value))
-  	.attr("transform", "translate(" + x(value) + ", 0)");
-  handle.attr("cx", x(value));
+
+  d3.select(".rrect").attr("width", xs(value));
+  d3.select(".jsrect").attr("width", width - xs(value))
+  	.attr("transform", "translate(" + xs(value) + ", 0)");
+
+  handle.attr("cx", xs(value));
 
   if (value < 60){
 	  d3.select("#second").transition(50).attr("opacity", 0);
